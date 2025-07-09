f428315037309b65c562b9035af4b6431234c9f0
Josua Friederichs <josua.friederichs@outlook.de>
2025-07-09 23:20:00 +0200

    Initial Commit

---
+++ pokedex-ios/pokedex_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_iosApp.swift
+//  pokedex-ios
+//
+//  Created by Josua Friederichs on 09.07.25.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
